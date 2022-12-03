a0bf3afd70533d955fc116ba5e3dc9e6aab541c6
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-03 20:35:13 +0900

    wait for a solution

--- huffman.h
+++ huffman.h
@@ -3,23 +3,29 @@
 #include"htree.h"
 typedef long double weight_t;
 template<class node_t>
-class node_val_t :public std::pair<node_t, weight_t> 
+class node_val_t :public std::pair<node_t, weight_t>
 {
 public:
-	node_val_t(void) { std::pair<node_t, weight_t>(0, 0); }
-	node_val_t(node_t data, weight_t weight = 0) { std::pair<node_t, weight_t>(data, weight); }
+	node_val_t(void) :
+		std::pair<node_t, weight_t>(0, 0)
+	{}
+	node_val_t(node_t data, weight_t weight = 0) :
+		std::pair<node_t, weight_t>(data, weight)
+	{}
 };
 template<class node_t>
 class node_val_list_t :public std::vector<node_val_t<node_t>>
 {
 public:
-	node_val_list_t(void) { std::vector<node_val_t<node_t>>(); }
-	node_val_list_t(node_val_t<node_t>child) { std::vector<node_val_t<node_t>>(child); }
-	node_val_list_t(node_t data, weight_t weight = 0)
-	{
-		node_val_t<node_t>t(data, weight);
-		node_val_list_t(t);
-	}
+	node_val_list_t(void) :
+		std::vector<node_val_t<node_t>>()
+	{}
+	node_val_list_t(node_val_t<node_t>child) :
+		std::vector<node_val_t<node_t>>(child)
+	{}
+	node_val_list_t(node_t data, weight_t weight = 0) :
+		node_val_list_t(node_val_t<node_t>(data, weight))
+	{}
 };
 template<class node_t>
 htree huffman(node_val_list_t<node_t> node_val_arg)
