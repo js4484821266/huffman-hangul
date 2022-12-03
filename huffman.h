c1e9b328b1c25636d1e0d6e814ee6cde103eb860
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-03 14:16:34 +0900

    typedef

--- huffman.h
+++ huffman.h
@@ -2,9 +2,11 @@
 #include<vector>
 #include"htree.h"
 template<class node_t>
-class node_val_t :std::pair<node_t, long double> {};
+class node_val_t :public std::pair<node_t, long double> {};
 template<class node_t>
-htree huffman(std::vector<node_val_t<node_t>>node_val_arg)
+class node_val_list_t :public std::vector<node_val_t<node_t>> {};
+template<class node_t>
+htree huffman(node_val_list_t<node_t> node_val_arg)
 {
 	htree tree;
 	return tree;
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include<vector>
 #include<iostream>
 #include"huffman.h"
+typedef node_val_t<wchar_t>hc_freq_t;
+typedef node_val_list_t<wchar_t>hc_freq_list_t;
 int main(void)
 {
+	hc_freq_list_t hfl;
 	return 0;
 }
