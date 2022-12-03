5ea43c288813ff7feb711bac2539b4121e9ecf38
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-03 17:18:47 +0900

    migrate from cpp to h

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include<vector>
 #include<iostream>
-extern class node_val_t;
-extern class node_val_list_t;
-typedef node_val_t<wchar_t>hc_freq_t;
-typedef node_val_list_t<wchar_t>hc_freq_list_t;
+#include"huffman.h"
+typedef node_val_t<wchar_t> hc_freq_t;
+typedef node_val_list_t<wchar_t> hc_freq_list_t;
 int main(void)
 {
 	hc_freq_list_t hfl;
