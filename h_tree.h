28c32df412083031adc7c9aa0464fa155a3132bb
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-06 21:07:27 +0900

    cleanup code

--- h_tree.h
+++ h_tree.h
@@ -3,29 +3,32 @@
 #include<vector>
 #include<string>
 
-namespace h_tree_t
+namespace js4484821266_h_tree
 {
-	// pair ( sign, data: subtree | term_data )
-	template <class TTermT>
-	using sub_tree_t = std::pair<std::wstring, TTermT>;
-
-	// array of children
-	template <class CTermT>
-	using children_t = std::vector<sub_tree_t<CTermT>>;
-}
-
-template <class ChildrenTT>
-class h_tree
-{
-public:
-	h_tree_t::children_t<ChildrenTT> children;
-
-	// outputs specification of this huffman tree
-	std::vector<std::wstring> str(void)
+	namespace h_tree_t
 	{
-		std::vector<std::wstring> o;
-		auto i = children.begin();
-		// your code here...
-		return o;
+		// pair ( sign, data: subtree | term_data )
+		template <class TTermT>
+		using sub_tree_t = std::pair<std::wstring, TTermT>;
+
+		// array of children
+		template <class CTermT>
+		using children_t = std::vector<sub_tree_t<CTermT>>;
 	}
-};
+
+	template <class ChildrenTT>
+	class h_tree
+	{
+	public:
+		h_tree_t::children_t<ChildrenTT> children;
+
+		// outputs specification of this huffman tree
+		std::vector<std::wstring> str(void)
+		{
+			std::vector<std::wstring> o;
+			auto i = children.begin();
+			// your code here...
+			return o;
+		}
+	};
+}
--- huffman.h
+++ huffman.h
@@ -2,16 +2,18 @@
 #include<vector>
 #include"h_tree.h"
 
-typedef long double weight_t;
-template <class NodeT>
-using node_val_t = std::pair<NodeT, weight_t>;
-template <class NodeT>
-using node_val_list_t = std::vector<node_val_t<NodeT>>;
+namespace js4484821266_huffman {
+	typedef long double weight_t;
+	template <class NodeT>
+	using node_val_t = std::pair<NodeT, weight_t>;
+	template <class NodeT>
+	using node_val_list_t = std::vector<node_val_t<NodeT>>;
 
-template <class NodeT>
-h_tree<NodeT> huffman(node_val_list_t<NodeT> node_val_arg)
-{
-	h_tree<NodeT> tree;
-	// your code here ...
-	return tree;
+	template <class NodeT>
+	js4484821266_h_tree::h_tree<NodeT> huffman(node_val_list_t<NodeT> node_val_arg)
+	{
+		js4484821266_h_tree::h_tree<NodeT> tree;
+		// your code here ...
+		return tree;
+	}
 }
